82e733969826618d2d923d13583406f45df2181a
unknown <congvtc02@gmail.com>
2024-01-05 00:51:47 +0700

    create intent switch login <-> register

--- app/src/main/java/com/btl/quizapp/LoginActivity.java
+++ app/src/main/java/com/btl/quizapp/LoginActivity.java
@@ -2,13 +2,26 @@ package com.btl.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView sign_up;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        sign_up = findViewById(R.id.text_switch_signup);
+
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
