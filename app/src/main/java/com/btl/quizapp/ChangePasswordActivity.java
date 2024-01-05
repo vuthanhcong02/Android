548ced988d37eead28e4e42b4daf12df6ab9f69f
unknown <congvtc02@gmail.com>
2024-01-05 15:48:33 +0700

    handle switch activities in navigation drawer

--- app/src/main/java/com/btl/quizapp/ChangePasswordActivity.java
+++ app/src/main/java/com/btl/quizapp/ChangePasswordActivity.java
@@ -1,14 +1,94 @@
 package com.btl.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class ChangePasswordActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    ImageView menu;
+    LinearLayout home, historyQuiz, changePassword,logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        menu = findViewById(R.id.menu);
+        home = findViewById(R.id.home);
+        historyQuiz = findViewById(R.id.history_quiz);
+        changePassword = findViewById(R.id.change_password);
+        logout = findViewById(R.id.logout);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDrawer(drawerLayout);
+            }
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectActivity(ChangePasswordActivity.this, MainActivity.class);
+            }
+        });
+        historyQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectActivity(ChangePasswordActivity.this, HistoryQuizActivity.class);
+            }
+        });
+        changePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectActivity(ChangePasswordActivity.this, ChangePasswordActivity.class);
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                redirectActivity(MainActivity.this, MainActivity.class);
+                Toast.makeText(ChangePasswordActivity.this, "Logout Success", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public static void openDrawer(DrawerLayout drawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+//    public void closeDrawer(DrawerLayout drawerLayout){
+//        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+//            drawerLayout.closeDrawer(GravityCompat.START);
+//        }
+//        else {
+//            super.onBackPressed();
+//        }
+//    }
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+    public static void redirectActivity(Activity activity, Class secondActivity) {
+        Intent intent = new Intent(activity, secondActivity);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+//        closeDrawer(drawerLayout);
+        onBackPressed();
     }
 }
