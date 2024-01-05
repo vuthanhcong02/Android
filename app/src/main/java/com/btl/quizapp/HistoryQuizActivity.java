34a69a3c79f60c7b18a8f8f47b1c5445e4680cbe
unknown <congvtc02@gmail.com>
2024-01-05 15:05:54 +0700

    create and design navigation drawer with activities

---
+++ app/src/main/java/com/btl/quizapp/HistoryQuizActivity.java
@@ -0,0 +1,14 @@
+package com.btl.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HistoryQuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_quiz);
+    }
+}
