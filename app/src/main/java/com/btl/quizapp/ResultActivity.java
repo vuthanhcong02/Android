14bbd4c0e19ba76fdcac8c5e9ac322d3f4d493a0
unknown <congvtc02@gmail.com>
2024-01-18 11:44:08 +0700

    .

--- app/src/main/java/com/btl/quizapp/ResultActivity.java
+++ app/src/main/java/com/btl/quizapp/ResultActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import com.btl.quizapp.database.DatabaseHelper;
-
 public class ResultActivity extends AppCompatActivity {
 
     @Override
